2bc13645fccb9a7c763d340fc4409702e1f28551
Sandun Induranga <sandutharu40@gmail.com>
2024-06-19 21:42:46 +0530

    Employee Schema Created

---
+++ src/employees/schemas/employees.schema.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProjectDocument = Employee & Document;
+
+@Schema()
+export class Employee{
+  @Prop({ required: true })
+  employeeId: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  address: string;
+
+  @Prop({ required: true })
+  birthday: Date;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  mobile: string;
+
+  @Prop({ required: true })
+  position: string;
+
+  @Prop({ required: true })
+  department: string;
+
+  @Prop({ required: true })
+  salary: number;
+
+  @Prop({ required: true })
+  gender: string;
+}
+
+export const EmployeeSchema = SchemaFactory.createForClass(Employee);
