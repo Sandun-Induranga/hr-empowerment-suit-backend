1cd0d1f8884e355fbb7b5729126e6e0c14e67ca9
Sandun Induranga <sandutharu40@gmail.com>
2024-08-17 00:36:08 +0530

    Auth Services Updated

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,39 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { CreateAuthDto } from './dto/create-auth.dto';
 
-describe('AuthService', () => {
-  let service: AuthService;
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+  async validateUser(email: string, pass: string): Promise<any> {
+    // const user = await this.usersService.findByEmail(email);
+    const user = await this.usersService.findByEmail(email);
+    if (user && bcrypt.compareSync(pass, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
 
-    service = module.get<AuthService>(AuthService);
-  });
+  async login(user: any) {
+    const payload = { email: user.email, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  async signUp(authDto: CreateAuthDto) {
+    const hashedPassword = bcrypt.hashSync(authDto.password, 10);
+    const user = await this.usersService.create({
+      ...authDto,
+      password: hashedPassword,
+    });
+    return this.login(user);
+  }
+}
